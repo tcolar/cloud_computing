a3b0781e3bd18378bf037cf53e1fb9dc7e6fe6ca
Thibaut Colar <tcolar@colar.net>
2017-07-04 08:50:00 -0700

    Another stabilization implementation, better but not great

--- concepts_part2/mp2/MP2Node.h
+++ concepts_part2/mp2/MP2Node.h
@@ -10,15 +10,14 @@
 /**
  * Header files
  */
-#include "stdincludes.h"
 #include "EmulNet.h"
-#include "Node.h"
 #include "HashTable.h"
 #include "Log.h"
-#include "Params.h"
 #include "Message.h"
+#include "Node.h"
+#include "Params.h"
 #include "Queue.h"
-
+#include "stdincludes.h"
 
 /**
  * CLASS NAME: MP2Node
@@ -32,70 +31,69 @@
  */
 class MP2Node {
 private:
-	// Vector holding the next two neighbors in the ring who have my replicas
-	vector<Node> hasMyReplicas;
-	// Vector holding the previous two neighbors in the ring whose replicas I have
-	vector<Node> haveReplicasOf;
-	// Ring
-	vector<Node> ring;
-	// Hash Table
-	HashTable * ht;
-	// Member representing this member
-	Member *memberNode;
-	// Params object
-	Params *par;
-	// Object of EmulNet
-	EmulNet * emulNet;
-	// Object of Log
-	Log * log;
-        // Transaction data
-        HashTable * trans;
-        // Transaction start time to keep track of timeouts
-        map<int, long> * transTimes;
+  // Vector holding the next two neighbors in the ring who have my replicas
+  vector<Node> hasMyReplicas;
+  // Vector holding the previous two neighbors in the ring whose replicas I have
+  vector<Node> haveReplicasOf;
+  // Ring
+  vector<Node> ring;
+  // Hash Table
+  HashTable *ht;
+  // Member representing this member
+  Member *memberNode;
+  // Params object
+  Params *par;
+  // Object of EmulNet
+  EmulNet *emulNet;
+  // Object of Log
+  Log *log;
+  // Transaction data
+  HashTable *trans;
+  // Transaction start time to keep track of timeouts
+  map<int, long> *transTimes;
 
 public:
-	MP2Node(Member *memberNode, Params *par, EmulNet *emulNet, Log *log, Address *addressOfMember);
-	Member * getMemberNode() {
-		return this->memberNode;
-	}
+  MP2Node(Member *memberNode, Params *par, EmulNet *emulNet, Log *log,
+          Address *addressOfMember);
+  Member *getMemberNode() { return this->memberNode; }
 
-	// ring functionalities
-	void updateRing();
-	vector<Node> getMembershipList();
-	size_t hashFunction(string key);
-	void findNeighbors();
+  // ring functionalities
+  void updateRing();
+  vector<Node> getMembershipList();
+  size_t hashFunction(string key);
+  void findNeighbors();
 
-	// client side CRUD APIs
-	void clientCreate(string key, string value);
-	void clientRead(string key);
-	void clientUpdate(string key, string value);
-	void clientDelete(string key);
+  // client side CRUD APIs
+  void clientCreate(string key, string value);
+  void clientRead(string key);
+  void clientUpdate(string key, string value);
+  void clientDelete(string key);
 
-	// receive messages from Emulnet
-	bool recvLoop();
-	static int enqueueWrapper(void *env, char *buff, int size);
+  // receive messages from Emulnet
+  bool recvLoop();
+  static int enqueueWrapper(void *env, char *buff, int size);
 
-	// handle messages from receiving queue
-	void checkMessages();
+  // handle messages from receiving queue
+  void checkMessages();
 
-	// coordinator dispatches messages to corresponding nodes
-	void dispatchMessages(Message message);
+  // coordinator dispatches messages to corresponding nodes
+  void dispatchMessages(Message message);
 
-	// find the addresses of nodes that are responsible for a key
-	vector<Node> findNodes(string key);
+  // find the addresses of nodes that are responsible for a key
+  vector<Node> findNodes(string key);
 
-	// server
-	bool createKeyValue(string key, string value, ReplicaType replica);
-	string readKey(string key);
-	bool updateKeyValue(string key, string value, ReplicaType replica);
-	bool deletekey(string key);
+  // server
+  bool createKeyValue(string key, string value, ReplicaType replica);
+  string readKey(string key);
+  bool updateKeyValue(string key, string value, ReplicaType replica);
+  bool deletekey(string key);
 
-	// stabilization protocol - handle multiple failures
-	void stabilizationProtocol();
+  // stabilization protocol - handle multiple failures
+  void stabilizationProtocol();
 
-        long now();
-        
-	~MP2Node();
+  long now();
+
+  ~MP2Node();
 };
 
 #endif /* MP2NODE_H_ */
