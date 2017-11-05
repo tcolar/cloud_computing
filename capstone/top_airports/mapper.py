5fd7410e8d325589953d3e96edf92ec478740a55
Thibaut Colar <tcolar@colar.net>
2017-11-04 17:21:33 -0700

    Mapper for top airports

---
+++ capstone/top_airports/mapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+ 
+import sys
+import csv
+ 
+for fields in csv.reader(iter(sys.stdin.readline, '')):
+    origin = fields[6]
+    dest = fields[14]
+    print(origin)
+    print(dest)
