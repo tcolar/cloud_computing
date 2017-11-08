b47f3de34e7ed354482e8b7f76c57c01385c6085
Thibaut Colar <tcolar@colar.net>
2017-11-08 07:37:36 -0800

    Delay per carrier and departure airport

---
+++ capstone/carrier_departures/delay_mapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+ 
+import sys
+import csv
+ 
+for fields in csv.reader(iter(sys.stdin.readline, '')):
+    carrier = fields[6]
+    origin = fields[11]
+    delay = fields[25]
+    if delay == 'DepDelay':
+	continue
+    if len(carrier)!=0 and len(origin)!=0 and len(delay)!=0:
+    	print("{}_{} {}".format(origin, carrier, delay))
